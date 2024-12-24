b962f55a1f9579114f9e41641d753956c7e351b4
VERITAS <50308392+veritas-rt@users.noreply.github.com>
2024-12-24 17:21:13 +0900

    Update BloodHound_E2J.sh

--- BloodHound_E2J.sh
+++ BloodHound_E2J.sh
@@ -4,14 +4,14 @@ PrebuiltQueries_Path="/usr/lib/bloodhound/resources/app/src/components/SearchCon
 CustomQueries_Path="${HOME}/.config/bloodhound/CustomQueries.json"
 
 # pre 
-mkdir -p ${HOME}/.config/bloodhound/
+mkdir -p "${HOME}/.config/bloodhound/"
 mkdir backups
 
 # BackUp
-cp $PrebuiltQueries_Path ./backups/PrebuiltQueries.json
-cp $CustomQueries_Path ./backups/CustomQueries.json
+cp "$PrebuiltQueries_Path" ./backups/PrebuiltQueries.json
+cp "$CustomQueries_Path" ./backups/CustomQueries.json
 
 # OverWrite
-sudo cp ./queries/PrebuiltQueries.json $PrebuiltQueries_Path
-cp ./queries/CustomQueries.json $CustomQueries_Path
+sudo cp ./queries/PrebuiltQueries.json "$PrebuiltQueries_Path"
+cp ./queries/CustomQueries.json "$CustomQueries_Path"
 
